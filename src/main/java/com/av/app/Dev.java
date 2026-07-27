07cfd327cf8bad1d88ece321702ac0d5da9ca903
adityakumardev <adityak.kumar78@gmail.com>
2026-07-27 15:59:50 +0530

    Manual Object Creation

---
+++ src/main/java/com/av/app/Dev.java
@@ -0,0 +1,10 @@
+package com.av.app;
+
+public class Dev {
+    
+    // a demo method to call
+
+    public void build() {
+        System.out.println("Build Started");
+    }
+}
--- src/main/java/com/av/app/app.java
+++ src/main/java/com/av/app/app.java
@@ -3,13 +3,16 @@ package com.av.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 // read about @SpringBootApplication in deep
 @SpringBootApplication
 public class app {
 
 	public static void main(String[] args) {
 		SpringApplication.run(app.class, args);
+		
+		// Created a custom object which user has to handle
+		Dev dev = new Dev();
+		dev.build();
 	}
 
 }
